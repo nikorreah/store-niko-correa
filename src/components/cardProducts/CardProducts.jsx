8df7a1daa1e97d866fb868001632acecba54a625
Nikorreah <nicolascorreah@gmail.com>
2021-08-18 22:36:42 -0500

    resolve the rendering for the products with .map

--- src/components/cardProducts/CardProducts.jsx
+++ src/components/cardProducts/CardProducts.jsx
@@ -1,49 +1,50 @@
 import { AppContext } from "../context/AppContext";
 import React, { useContext, useEffect } from "react";
-import { URLProducts } from "../utils/utilities"
+import { URLProducts, headers } from "../utils/utilities"
 
 import "./cardProducts.css"
 
 function CardProducts() {
-    const { productData, setProductData } = useContext(AppContext)
+    const useFetch = (URLProducts) => {
+        const { productData, setProductData } = useContext(AppContext)
 
-    const getProduct = async (setProductData) => {
-        try {
-            const response = await fetch(URLProducts, {
-                method: 'GET', headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTBjOTc5NWQwMDVjZDAwMjE0NDc3MDkiLCJpYXQiOjE2MjgyMTUxODl9.WnOZ5f3lMVnjsX3VI8JKQlCOI3nf1Nu6IhtkdykdsfI',
-                    redirect: "follow",
+        useEffect(() => {
+            if (productData.length === 0) {
+                async function getproduct() {
+                    const request = async () => await fetch(URLProducts, headers);
+                    try {
+                        const response = await request();
+                        const data = await response.json();
+                        console.log(data);
+                        setProductData(data);
+                    } catch (error) {
+                        console.log(error, 'error here')
+                    }
                 }
-            });
-            const data = await response.json();
-            setProductData(data);
-        } catch (error) {
-            console.log("error", error)
-        }
-    }
-    useEffect(() => {
-        if (productData.length === 0) {
-            getProduct(setProductData);
-        }
-    }, [productData, setProductData])
+                getproduct();
+            }
+        }, [productData, setProductData, URLProducts]);
+        return { productData };
+    };
+
+    const { productData } = useFetch(URLProducts)
 
     return (
-        <div>
-             {productData.map((products)=> {
-                <div className="card-container">
-                    <div className="pic-product-containe">
-                        <img src={products.img.url} alt={products.name} key={products._id} border="0" className="pic-size" />
-                    </div>
-                    <hr className="center-line"></hr>
-                    <div className="text-container">
-                        <h5 className="category-text">{products.category}</h5>
-                        <h3 className="product-text ">{products.name}</h3>
-                    </div>
-                </div>
-
-            })}
+        <div className="product-container">
+            {productData.map((prod) => {
+                return (
+                    <div className="card-container" key={prod._id} >
+                        <div className="pic-product-containe">
+                            <img src={prod.img.hdUrl} alt={prod.name} key={prod._id} border="0" className="pic-size" />
+                        </div>
+                        <hr className="center-line"></hr>
+                        <div className="text-container">
+                            <h5 className="category-text">{prod.category}</h5>
+                            <h3 className="product-text ">{prod.name}</h3>
+                        </div>
+                    </div>)
+            })
+            }
         </div>
     )
 }
--- src/components/utils/utilities.js
+++ src/components/utils/utilities.js
@@ -1,5 +1,19 @@
-export const URLUser= ('https://coding-challenge-api.aerolab.co/user/me')
+export const URLUser = ('https://coding-challenge-api.aerolab.co/user/me')
+export const headers = {
+    method: 'GET', headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTBjOTc5NWQwMDVjZDAwMjE0NDc3MDkiLCJpYXQiOjE2MjgyMTUxODl9.WnOZ5f3lMVnjsX3VI8JKQlCOI3nf1Nu6IhtkdykdsfI',
+    },
+}
+    export const URLProducts = ('https://coding-challenge-api.aerolab.co/products')
+
+
+
+
+
+
 // const URLPoints= 'https://coding-challenge-api.aerolab.co/user/points'
 // const URLHistory= 'https://coding-challenge-api.aerolab.co/user/history'
 // const URLRedeem= 'https://coding-challenge-api.aerolab.co/redeem'
-export const URLProducts= ('https://coding-challenge-api.aerolab.co/products')
+
